907babbf317d098e8097a9694188c7b4ea157132
Harshit Tiwari <harshitoff18@gmail.com>
2025-11-04 11:46:49 +0530

    Time: 52 ms (32.87%), Space: 46.5 MB (24.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,25 +1,37 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-      int n = nums.length;
-      int[] dp = new int[n];
-      Arrays.fill(dp,1);
-      for(int i=1;i<n;i++) {
-        for(int j=0;j<i;j++) {
-            if(nums[i]>nums[j]) {
-                if(dp[j]+1>dp[i]) {
-                    dp[i]=dp[j]+1;
+    public int lengthOfLIS(int[] arr) {
+        int n=arr.length;
+        int[] parent=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i] > arr[j]){
+                    if(dp[j]+1 > dp[i]){
+                        dp[i]=dp[j]+1;
+                        parent[i]=j;
+                    }
                 }
-            } 
+            }
         }
-      }
-
-      int max=-1;
-      for(int i=0;i<n;i++) {
-        if(dp[i]>max) {
-            max=dp[i];
+        int max=-1;
+        int li=0;
+        for(int i=0;i<n;i++){
+            if(dp[i] > max){
+                max=dp[i];
+                li=i;
+            }
         }
-      }
-
-      return max;
+        List<Integer> lis=new ArrayList<>();
+        lis.add(arr[li]);
+        while(parent[li] != li){
+            li=parent[li];
+            lis.add(arr[li]);
+        }
+        System.out.println(lis);
+       return max;
     }
 }
