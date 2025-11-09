767a548b436d4105ba367eb495e871bd765cdf81
Harshit Tiwari <harshitoff18@gmail.com>
2025-11-09 12:05:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (5.15%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -3,22 +3,13 @@ class Solution {
         int[][] dp = new int[m][n];
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                dp[i][j] = -1;
+                if(i==0 || j==0) dp[i][j] = 1;
+                else {
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                }
             }
         }
 
-        return ways(0,0,m,n,dp);
+        return dp[m-1][n-1];
     }
-
-    public int ways(int r,int c,int m,int n, int[][] dp) {
-        if(r==m-1 && c==n-1) return 1;
-        if(r>=m || c>=n) return 0;
-
-        if(dp[r][c] != -1) return dp[r][c];
-
-        int rightWays = ways(r,c+1,m,n,dp);
-        int downWays = ways(r+1,c,m,n,dp);
-
-        return dp[r][c] = rightWays+downWays;
-    } 
 }
